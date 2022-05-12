780e43a5c235b688ce6f715690ae946b8968a4a9
Ali-Joon <48525075+Ali-Joon@users.noreply.github.com>
2022-05-12 11:41:52 +0430

    Create app.php

---
+++ messages/fa/app.php
@@ -0,0 +1,236 @@
+<?php
+return [
+    'Ascending' => 'صعودی',
+    'Add New Server' => 'افزودن سرور جدید',
+    'Add Your Server' => 'افزودن سرور شما',
+    'Add Bot' => 'اضافه کردن بات',
+    'Add a Tag' => 'افزودن یک برچسب',
+    'Add <b>CREATE_INSTANT_INVITE</b> and <b>SEND_MESSAGES</b> permissions to DISBOARD bot.' => 'به بات دیسبورد دسترسی های <b>CREATE_INSTANT_INVITE</b> و <b>SEND_MESSAGES</b> را بدهید.',
+    'Admin response' => 'پاسخ ادمین',
+    'Admin servers' => 'سرور های ادمین',
+    'After that send this command: <span class="tag is-light">!d invite</span>' => 'بعد از آن، این دستور را اجرا کنید: <span class="tag is-light">!d invite</span>',
+    'All Discord reviews' => 'تمامی نظرات دیسکورد',
+    'All Discord servers' => 'تمام سرور های دیسکورد',
+    'All shards are operational!' => 'همه شارد ها درحال انجام کار هستند!',
+    'Are you sure you want to delete {serverName}?' => 'آیا مطمئن هستید که میخواهید {serverName} را حذف کنید؟',
+    'Are you sure you want to delete this review?' => 'آیا مطمئن هستید که میخواهید این سرور را حذف کنید؟',
+    'Bad Request' => 'درخواست نامعتبر',
+    'Be positive, constructive and respectful' => 'مثبت، محترم و سازنده باشید',
+    'Bot Status' => 'وضعیت بات',
+    'Bump' => 'اضافه کردن',
+    'Bumped At' => 'اضافه شده در',
+    'Bumped {time}' => 'اضافه شد {time}',
+    'Bumped recently' => 'اخیراً اضافه شده',
+    'Cancel' => 'لغو',
+    'Categorize your server by keywords (e.g. game title)' => 'سرور خود را با کلید واژه ها دسته‌بندی کنید (مثلا عنوان بازی)',
+    'Category' => 'دسته‌بندی',
+    '<b>Categories</b> and <b>Popular tags</b>' =>  '<b>دسته‌بندی ها</b> و <b>برچسب های محبوب</b>',
+    'Copied the URL' => 'آدرس URL کپی شد',
+    'Could not bump the server because the invite link is invalid (expired?)' => 'نتوانستید سرور را اضافه کنید بخاطر اینکه لینک سرور نامعتبر (منقضی شده) است؟',
+    'Could not delete the server' => 'ناتوانی در حذف این سرور',
+    'Could not delete the review' => 'ناتوانی در حذف این نظر',
+    'Created at {datetime}' => 'ساخته شده در {datetime}',
+    'Dashboard' => 'داشبورد',
+    'Descending' => 'نزولی',
+    'Description' => 'توضیحات',
+    'Delete server' => 'حذف سرور',
+    'Delete reply' => 'حذف پاسخ',
+    'Delete review' => 'حذف نظر',
+    'DISBOARD bot API server is offline' => 'API بات دیسبورد آفلاین است',
+    'DISBOARD bot must be in the server to review' => 'بات دیسبورد باید در سرور باشد تا بتوان نظر داد',
+    'Discord Server List' => 'لیست سرور دیسکورد',
+    'Discord Server Reviews' => 'لیست نظرات دیسکورد',
+    'Discord Servers Bumped Recently' => 'سرور های دیسکوردی که اخیراٌ اضافه شده‌اند',
+    'Discord servers categorized as <strong>{category}</strong>' => 'سرور هایی که به عنوان <strong>{category}</strong> دسته‌بندی شده‌اند',
+    'Discord servers tagged with {tag}' => 'سرور هایی که برچسب {tag} را دارند',
+    '<span class="discord-logo">Discord</span> Servers' => 'سرور های <span class="discord-logo">دیسکورد</span>',
+    'Discord servers tagged with <strong>{tag}</strong>' => 'سرور هایی که برچسب <strong>{tag}</strong> را دارند',
+    'Don\'t forget to check "Set this link to never expire"' => 'فراموش نکنید که "Set this link to never expire"\' را فعال کرده باشید!',
+    'Edit' => 'ویرایش',
+    'Edit Server' => 'ویرایش سرور',
+    'Enter keyword(s)' => 'کلیدواژه(ها) را وارد کنید',
+    'Failed to Sync' => 'شکست در همگام‌سازی',
+    'Games' => 'بازی ها',
+    'Gaming' => 'بازی',
+    'Get link' => 'گرفتن لینک',
+    'Go to Dashboard' => 'به داشبورد بروید',
+    'Go Back' => 'بازگشت',
+    'Go Home' => 'بازگشت به صفحه اصلی',
+    'Guidelines' => 'شرایط و ضوابط',
+    'Home' => 'صفحه اصلی',
+    'If you don\'t add a bot, you can NOT present various informations about your server, create an invitation link or bump up your servers with commands' => 'اگر بات را اضافه نکنید، شما نمی توانید اطلاعات مختلفی درباره سرور خود ارائه دهید، یک لینک دعوت ایجاد کنید یا سرور خود را با این دستورات اضافه کنید',
+    'If your server isn\'t in the above list or dropdown, sync with Discord manually' => 'اگر سرور شما در لیست بالا یا در منوی پایین نیست، با دیسکورد به طور دستی آن را هماهنگ سازی کنید',
+    'Invite is not set' => 'لینک دعوت تنظیم نشده',
+    'You can now write reviews and add ratings for servers you are joining.<br>Let\'s recommend your favorite servers to other people!' => 'حال شما می توانید درباره سرور هایی که به آنها می پیوندید، نظرات خود را بنویسید و به آنها امتیاز دهید. <br>بیاید سرور های محبوبتان را به بقیه افراد معرفی کنیم!',
+    'Invite Link' => 'لینک دعوت',
+    'Invalid Invite' => 'لینک دعوت نامعتبر',
+    'Join this Server' => 'به این سرور بپیوندید',
+    'Join this server to be the first reviewer!' => 'به این سرور بپیوندید و اولین نفری باشید که درباره آن نظری ارائه می کند!',
+    'Language' => 'زبان',
+    'Login' => 'ورود',
+    'Logout' => 'خروج',
+    'Main Language' => 'زبان اصلی',
+    'Member Count' => 'تعداد اعضاء',
+    '<b>{number}</b> online' => '<b>{number}</b> عضو آنلاین', // online member count
+    '<b>{number}</b> total' => '<b>{number}</b> اعضاء کل', // toal member count
+    '{time, plural, =1{# minute} other{# minutes}}' => '{time} دقیقه',
+    'New!' => 'جدید!',
+    'New Review' => 'نظر جدید',
+    'Maximum number of muted servers exceeded. To mute a server, go to Settings and unmute another server.' => 'به حداکثر تعداد سرور های میوت شده رسیدید. برای میوت کردن یک سرور، بروید به تنظیمات و یک سرور دیگر را از حالت میوت در بیاورید.',
+    'More about {appName}' => 'اطلاعات بیشتر درباره {appName}',
+    'Must be checked if the server\'s main theme is NSFW, such as sexually explicit content' => 'درصورتی که موضوع اصلی سرور NSFW می باشد (مثل محتوای جنسی صریح)، این مورد باید فعال باشد',
+    'Mute this server' => 'این سرور را میوت کن',
+    'Muted servers' => 'سرور های میوت شده',
+    'Muted {serverName}' => '{serverName} میوت شده',
+    'Not Found' => 'یافت نشد',
+    'No need for <span>#</span> at the beginning' => 'هیچ نیازی به <span>#</span> در ابتدا نیست',
+    'No reviews and ratings yet' => 'بدون هیچ نظر و امتیاز',
+    'No results found for <strong>{keyword}</strong>' => 'نتیجه‌ای برای <strong>{keyword}</strong> یافت نشد',
+    'NSFW' => 'NSFW (+18)',
+    'NSFW server' => 'سرور NSFW (+18)',
+    'Official Discord Server' => 'سرور رسمی دیسکورد',
+    'Owner servers' => 'سرور های دارنده',
+    'Please check the checkbox below before bumping "<strong>{serverName}</strong>"' => 'لطفا قبل از اضافه کردن "<strong>{serverName}</strong>" به لیست، چک‌باکس زیر را بررسی کنید',
+    'Please check the checkbox below before joining "<strong>{serverName}</strong>"' => 'لطفا قبل از پیوستن به "<strong>{serverName}</strong>"، چک‌باکس زیر را بررسی کنید',
+    'Please enter no more than {tagLimit} tags.' => 'لطفا بیشتر از {tagLimit} برچسب وارد نکنید.',
+    'Please select a server' => 'لطفا یک سرور را انتخاب کنید',
+    'Please wait another {0, plural, =1{# minute} other{# minutes}} until the server can be bumped' => 'لطفا {0} دقیقه دیگر برای اضافه شدن سرور صبر کنید',
+    'Please wait another {0, plural, =1{# minute} other{# minutes}} until you can bump' => 'لطفا {0} دقیقه دیگر برای اضافه کردن سرور صبر کنید',
+    'Popular Tags' => 'برچسب های محبوب',
+    'Private' => 'خصوصی',
+    'Public' => 'عمومی',
+    'Public Discord Servers' => 'سرور های عمومی دیسکورد',
+    'Public Discord Server List' => 'لیست سرور های عمومی دیسکورد',
+    'Rating' => 'امتیاز',
+    'Ratings & Reviews' => 'امتیاز ها و نظرات',
+    'Read more' => 'بیشتر بخوانید',
+    'reCAPTCHA failed' => 'انجام reCAPTCHA با شکست مواجه شد',
+    'Recommended' => 'توصیه‌شده',
+    'Redirecting' => 'درحال ارجاع',
+    'Reply' => 'پاسخ',
+    'Reply to Review' => 'پاسخ به نظر',
+    'Report Description' => 'توضیحات گزارش',
+    'Report Server' => 'گزارش سرور',
+    'Report this server' => 'گزارش دادن این سرور',
+    'Report Type' => 'نوع گزارش',
+    'Reset Filters' => 'بازنشانی فیلتر ها',
+    'Review' => 'نظر',
+    'Review should be personal feedback only. <br>Do not harm someone\'s reputation, whether it is fact or not.' => 'نظر فقط باید بازخورد شخصی باشد. به هیچ عنوان به اعتبار کسی آسیب نزنید، حتی اگر این حقیقت محض باشد.',
+    'Reviews' => 'نظرات',
+    'Reviewed by' => 'نظر ارسال شده توسط',
+    '{count, plural, =1{<b>#</b> review} other{<b>#</b> reviews}}' => '{count} نظر',
+    'Review {serverName}' => 'دادن نظر درباره {serverName}',
+    'Reviews of {serverName}' => 'نظرات {serverName}',
+    'Save' => 'ذخیره',
+    'See: <a href="{url}" target="_blank">screen capture</a>' => '<a href="{url}" target="_blank">عکس صفحه</a> را ببینید',
+    'Search' => 'جستجو',
+    'Search Discord Servers' => 'جستجو در سرور های دیسکورد',
+    'Search Options' => 'تنظیمات جستجو',
+    'Search results for: {keyword}' => 'نتایج برای {keyword}',
+    'Select a Category' => 'یک دسته‌بندی انتخاب کنید',
+    'Select Language' => 'انتخاب زبان',
+    'Select a Server' => 'یک سرور انتخاب کنید',
+    'Select a server to add to the public server list on DISBOARD' => 'یک سرور را برای افزودن به لیست سرور های عمومی در دیسبورد، انتخاب کنید',
+    'Select a server to review' => 'یک سرور را برای نظر دادن انتخاب کنید',
+    'Send the invite command: `!d invite #channel`' => 'دستور لینک دعوت را اجرا کنید: `!d invite #channel`',
+    'Separate by <b>space</b>' => 'با <b>دکمه فاصله</b> جدا کنید',
+    'Settings' => 'تنظیمات',
+    'Servers' => 'سرور ها',
+    'Server admin?' => 'آیا ادمین سرور هستید؟',
+    'Server Language' => 'زبان سرور',
+    '{num, plural, =1{# shard is dead} other{# shards are dead}}' => '{num} شارد از کار افتاده',
+    'Showing <b>{from}</b> - <b>{to}</b> of <strong>{total} servers</strong>' => 'نمایش <b>{from}</b> - <b>{to}</b> از <strong>{total}سرور</strong>',
+    'Showing <b>{from}</b> - <b>{to}</b> of <strong>{total} reviews</strong>' => 'نمایش <b>{from}</b> - <b>{to}</b> از <strong>{total}نظر</strong>',
+    'Sort by' => 'مرتب سازی بر اساس',
+    'Submit' => 'ثبت',
+    'Support' => 'پشتیبانی',
+    'Sync servers' => 'همگام سازی سرور ها',
+    'Synced with Discord' => 'همگام سازی شده با دیسکورد',
+    'Tags' => 'برچسب ها',
+    'Tags similar to {tag}' => 'برچسب های مشابه به {tag}',
+    'Terms of Service' => 'شرایط استفاده',
+    'The invite of this server is invalid or has expired.' => 'لینک دعوت این سرور نامعتبر بوده یا اینکه منقضی شده است.',
+    'The report has been sent' => 'گزارش ارسال شد',
+    'The server is set to private' => 'سرور روی حالت خصوصی تنظیم شد',
+    'This server has been removed due to breaking our guidelines' => 'این سرور بابت شکست قوانین استفاده از سرویس ما، حذف شد',
+    'This server is private. Only the owner (you) can view it.' => 'این سرور خصوصی است. فقط مالک آن (شما) می توانید آن را ببینید.',
+    'This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.' => 'این سایت توسط reCAPTCHA محافظت شده و <a href="https://policies.google.com/privacy">شرایط حریم خصوصی</a> و <a href="https://policies.google.com/terms"> شرایط استفاده از سرویس</a> های گوگل نیز روی این سایت اعمال می شوند.',
+    'This review has been removed due to breaking our guidelines' => 'این نظر بابت شکست قوانین ما، حذف شد',
+    'This will add this server to our public server list' => 'این سرور به لیست سرور های عمومی ما اضافه خواهد شد',
+    'Title' => 'عنوان',
+    '{tag1} and {tag2}(nsfw) tags cannot be set together' => 'برچسب {tag1} و برچسب {tag2}(NSFW) نمی توانند باهم استفاده شوند',
+    '<b>{total}</b> servers in total' => '<b>{total}</b> سرور در کل',
+    '<b>{total}</b> reviews in total' => '<b>{total}</b> نظر در کل',
+    'Unmute' => 'از میوت درآوردن',
+    'Unmuted {serverName}' => 'از میوت درآورده شد {serverName}',
+    'Unspecified' => 'مشخص نشده',
+    'Up to {limit} tags' => 'تا حداکثر {limit} برچسب',
+    'User' => 'کاربر',
+    'Users' => 'کاربران',
+    'View' => 'مشاهده', // view server button on Dashboard
+    'Why don\'t you review your favorite <span class="discord-logo">Discord</span> server?' => 'چرا شما به سرور <span class="discord-logo">دیسکورد</span> محبوب خود نظر ندهید؟',
+    'Why don\'t you <a href="{reviewUrl}">review <strong>{serverName}</strong></a>?' => 'چرا به سرور <a href="{reviewUrl}"><strong>{serverName}</strong> نظر</a> ندهید؟',
+    'you are not alone' => 'شما تنها نیستید',
+    'You have already muted {serverName}' => 'شما از قبل سرور {serverName} را میوت کرده‌اید',
+    'You haven\'t added a server yet' => 'شما هنوز سروری اضافه نکرده‌اید',
+    'You must be logged in to mute a server' => 'شما بایستی برای میوت کردن یک سرور، وارد شده باشید',
+    'You must be a member of the server to review' => 'شما برای نوشتن یک نظر، بایستی عضو سرور باشید',
+    'You need to be in the server for at least {duration} days to review' => 'شما برای نوشتن یک نشر، بایستی حداقل {duration} روز عضو سرور بوده باشید',
+    'You need to follow our <a href="{url}" target="_blank">Guidelines</a>' => 'شما بایستی از <a href="{url}" target="_blank">شرایط و ضوابط</a> پیروی کنید',
+    'You seem like a robot' => 'به نظر میاید که شما یک ربات هستید',
+    'Your servers' => 'سرور های شما',
+    'Your reviews' => 'نظرات شما',
+    'Your server has been bumped' => 'سرور شما به لیست اضافه شد',
+    'Your server has been deleted' => 'سرور شما از لیست حذف شد',
+    'Your server has been saved successfully' => 'سرور شما با موفقیت ذخیره شد',
+    'Your reply has been deleted' => 'پاسخ شما حذف شد',
+    'Your reply has been saved successfully' => 'پاسخ شما با موفقیت ذخیره شد',
+    'Your review has been deleted' => 'نظر شما حذف شد',
+    'Your review has been saved successfully' => 'نظر شما با موفقیت ذخیره شد',
+    'just now' => 'تقریباً همین الان',
+    '{0, plural, =1{# day} other{# days}} ago' => '{0} روز پیش',
+    '{0, plural, =1{# hour} other{# hours}} ago' => '{0} ساعت پیش',
+    '{0, plural, =1{# minute} other{# minutes}} ago' => '{0} دقیقه پیش',
+    '{0, plural, =1{# second} other{# seconds}} ago' => '{0} ثانیه پیش',
+    'DISBOARD was made to make everyone enjoy Discord more.<br>We hope you find awesome Discord servers and friends here 😊.' => 'دیسبورد برای این درست شد که همه از دیسکورد بیشتر لذت ببرند. <br>امیدواریم که سرور های خوب و دوستان صمیمی از اینجا برای خود در دیسکورد پیدا کنید 😊.',
+    /* Rules, ToS */
+    'Name or description contains insult or swear words' => 'نام یا توضیحات حاوی کلمات زشت و فحش هستند',
+    'Spam / fake server' => 'اسپم / سرور تقلبی',
+    'Invite is invalid or expired' => 'لینک دعوت منقضی شده یا معتبر نیست',
+    /* Welcome Messages on home page */
+    'Discover <span class="discord-logo">Discord</span> Servers' => 'سرور های <span class="discord-logo">دیسکورد</span> پیدا کنید',
+    'We connect <strong>Discord servers</strong> and <b>people</b> together' => 'ما <strong>سرور های دیسکورد</strong> و <b>مردم</b> را داریم به هم نزدیک تر می کنیم',
+    '<strong class="brand">DISBOARD</strong> is the place where you can <strong>list/find Discord servers</strong>.' => '<strong class="brand">دیسبورد</strong> یک جا برای <strong>پیدا کردن/اضافه کردن سرور های دیسکورد</strong> می باشد',
+    'Find and join some awesome servers listed here.' => 'از سرور هایی که اینجا لیست شده‌اند، سرور های خوبی پیدا کنید و به آنها بپیوندید.',
+    'Or <a href="{login-url}">login</a> and <strong>add your server if you are a server owner</strong> 😎' => 'یا اینکه <a href="/login">وارد شوید</a> و <strong>درصورتی که مالک یک سرور هستید، آن را اضافه کنید 😎</strong>',
+    'Add Your Server!' => 'اضافه کردن سرور شما!', // The large button
+    /* Categories */
+    'Adventure Games' => 'بازی های ماجراجویی',
+    'All Games' => 'همه بازی ها',
+    'Anime · Manga' => 'انیمه - مانگا',
+    'Community' => 'انجمن',
+    'Computers' => 'کامپیوتر',
+    'FPS Games' => 'بازی های FPS',
+    'MMO Games' => 'بازی های MMO',
+    'MOBA Games' => 'بازی های MOBA',
+    'Movies' => 'فیلم و سینما',
+    'Music' => 'موسیقی',
+    'Other' => 'دیگر',
+    'Party Games' => 'بازی های جمعی',
+    'Programming' => 'برنامه نویسی',
+    'Puzzle Games' => 'بازی های فکری',
+    'RPG Games' => 'بازی های RPG',
+    'Retro Games' => 'بازی های قدیمی',
+    'Sandbox Games' => 'بازی های Sandbox',
+    'Simulation Games' => 'بازی های شبیه‌سازی',
+    'Sports Games' => 'بازی های ورزشی',
+    'Strategy Games' => 'بازی های استراتژی',
+    'Technology' => 'تکنولوژی',
+    'TCG Games' => 'بازی های TCG',
+    'Tabletop Games' => 'بازی های رومیزی',
+    'Fighting Games' => 'بازی های مبارزه‌ای',
+    /* Meta Descriptions */
+    'DISBOARD is the public Discord server listing community. You can search Discord servers by your interest like Gaming, Anime, Music, etc. Find and join some awesome servers listed here!' => 'دیسبورد یک انجمن لیست کردن سرور های عمومی برای دیسکورد است. شما می توانید سرور های دیسکورد متناسب با سلیقه خود را در موضوعاتی مثل بازی، انیمه، موسیقی و... جستجو کنید. بچند تا از این سرور هایی که لیست شده‌‌اند را پیدا کنید و به آنها بپیوندید!',
+    'Search results for: {keyword}. There {count, plural, =1{is # server which is} other{are # servers which are}} waiting for your joining!' => 'نتایج جستجو برای: {keyword}. {count} سرور منتظر پیوستن شما هستند!',
+    'List of Discord servers tagged with {tag}. Find and join some awesome servers listed here!' => 'لیست سرور های با برچسب {tag}. از اینجا چند تا سرور خوب پیدا کنید و به آنها بپیوندید!',
+];
